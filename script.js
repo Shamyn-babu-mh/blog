9d2c896b12d885079678aa15ace96f40887cc371
Shamyn-babu-mh <shamyn.babu.mh@gmail.com>
2026-07-20 14:52:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function showContact () {
-  document.getElementById('contactScreen').style.display = "block";
-  document.getElementById('close').style.display = "block";
-}
 
-function hideContact () {
-  document.getElementById('contactScreen').style.display = "none";
-  document.getElementById('close').style.display = "none";
-  
-}
